0d5c8b863f43449801ce9d0dc019b01b3fd83cda
Karine Santos <jkss13@discente.ifpe.edu.br>
2023-05-19 21:37:02 -0300

    +1 modificacao

--- src/GCSM/HelloWorld.java
+++ src/GCSM/HelloWorld.java
@@ -6,6 +6,7 @@ public class HelloWorld {
         System.out.println("Hello, World!"); 
         System.out.println("Modificação");
         System.out.println("Outra modificação");
+        System.out.println("Mais uma modificação");
     }
 
 }
